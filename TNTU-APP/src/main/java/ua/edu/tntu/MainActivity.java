c45cd73a5794dd3da0106b1f1637f51b8c695633
hofmn <paul.hofmann@yandex.ua>
2013-10-15 19:52:41 +0300

    Update UI
    Changed tabs

--- TNTU-APP/src/main/java/ua/edu/tntu/MainActivity.java
+++ TNTU-APP/src/main/java/ua/edu/tntu/MainActivity.java
@@ -1,105 +1,85 @@
 package ua.edu.tntu;
 
-import android.annotation.TargetApi;
-import android.app.ActionBar;
-import android.app.FragmentTransaction;
-import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.TypedValue;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 
-import ua.edu.tntu.navigation.AppSectionsPagerAdapter;
+public class MainActivity extends FragmentActivity {
 
-public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
+    private PagerSlidingTabStrip tabs;
+    private ViewPager pager;
+    private MyPagerAdapter adapter;
 
-    private ViewPager viewPager;
 
-    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-    public void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Create the adapter that will return a fragment for each of the three primary sections
-        // of the app.
-        AppSectionsPagerAdapter sectionsPagerAdapter = new AppSectionsPagerAdapter(getSupportFragmentManager());
+        tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        pager = (ViewPager) findViewById(R.id.pager);
+        adapter = new MyPagerAdapter(getSupportFragmentManager());
 
-        // Set up the action bar.
-        final ActionBar actionBar = getActionBar();
+        pager.setAdapter(adapter);
 
-        // Specify that the Home/Up button should not be enabled, since there is no hierarchical
-        // parent.
-        assert actionBar != null;
-        actionBar.setHomeButtonEnabled(true);
+        final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
+                .getDisplayMetrics());
+        pager.setPageMargin(pageMargin);
 
-        // Specify that we will be displaying tabs in the action bar.
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        tabs.setViewPager(pager);
 
-        //actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#34495e")));
-
-        // Set up the ViewPager, attaching the adapter and setting up a listener for when the
-        // user swipes between sections.
-        viewPager = (ViewPager) findViewById(R.id.pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                // When swiping between different app sections, select the corresponding tab.
-                // We can also use ActionBar.Tab#select() to do this if we have a reference to the
-                // Tab.
-                actionBar.setSelectedNavigationItem(position);
-            }
-        });
-
-        // For each of the sections in the app, add a tab to the action bar.
-
-//        actionBar.addTab(actionBar.newTab().setIcon(R.drawable.news_tab_icon).setTabListener(this));
-//        actionBar.addTab(actionBar.newTab().setIcon(R.drawable.calendar_tab_icon).setTabListener(this));
-//        actionBar.addTab(actionBar.newTab().setIcon(R.drawable.info_tab_icon).setTabListener(this));
-
-        actionBar.addTab(actionBar.newTab().setText("News").setTabListener(this));
-        actionBar.addTab(actionBar.newTab().setText("Schedule").setTabListener(this));
-        actionBar.addTab(actionBar.newTab().setText("Info").setTabListener(this));
     }
 
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu items for use in the action bar
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main, menu);
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle presses on the action bar items
+        return super.onOptionsItemSelected(item);
+    }
 
-        // TODO: do something with that
+    public class MyPagerAdapter extends FragmentPagerAdapter {
 
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                openOptionsMenu();
-                //Toast.makeText(this, "You Push Back", Toast.LENGTH_SHORT).show();
-            default:
-                return super.onOptionsItemSelected(item);
+        private final String[] TITLES = {"Новини", "Розклад", "Про кафедру"};
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
         }
-    }
 
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    }
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TITLES[position];
+        }
 
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-        // When the given tab is selected, switch to the corresponding page in the ViewPager.
-        viewPager.setCurrentItem(tab.getPosition());
-    }
+        @Override
+        public int getCount() {
+            return TITLES.length;
+        }
 
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new NewsFragment();
+
+                case 1:
+                    return new ScheduleFragment();
+
+                case 2:
+                    return new InfoFragment();
+
+                default:
+                    return null;
+            }
+        }
     }
 }
